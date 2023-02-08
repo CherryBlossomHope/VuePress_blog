ea0b410df6ed0443c4a7124eb21b3edf2c192917
Mashiro <1526566980@qq.com>
2023-02-08 17:13:09 +0800

    vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,29 @@
+import { defineUserConfig, defaultTheme } from 'vuepress'
+
+export default defineUserConfig({
+    lang: 'zh-CN',
+    title: 'Shiina Mashiro',
+    base: '/wtt.github.io/',
+    description: '这是我的第一个 VuePress 站点',
+    head: [['link', { rel: 'icon', href: '/wtt.github.io/img/logo.png' }]],
+    theme: defaultTheme({
+        navbar: [
+            {
+                text: '首页',
+                link: '/'
+            },
+            {
+                text: 'GitHub',
+                link: 'https://github.com/CherryBlossomHope'
+            }
+        ],
+        sidebar: {
+            '/guide/': [
+                {
+                    text: 'Guide',
+                    children: ['/guide/README.md', '/guide/getting-started.md'],
+                },
+            ],
+        },
+    })
+})
